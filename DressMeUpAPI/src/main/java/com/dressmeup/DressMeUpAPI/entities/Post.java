bbd6850399ba015b8e1e0484a215c676475d0427
bartpob <bartlomiejpoblocki@gmail.com>
2024-04-26 20:24:58 +0200

    add post entity

---
+++ DressMeUpAPI/src/main/java/com/dressmeup/DressMeUpAPI/entities/Post.java
@@ -0,0 +1,49 @@
+package com.dressmeup.DressMeUpAPI.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "POSTS")
+public class Post {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    @Column(name = "TEXT", length = 200)
+    private String text;
+
+    @Getter
+    @Setter
+    @Column(name = "DATE")
+    private Date date;
+
+    @Getter
+    @Setter
+    @Column(name = "LATITUDE")
+    private Double latitude;
+
+    @Getter
+    @Setter
+    @Column(name = "LONGITUDE")
+    private Double longitude;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Getter
+    @Setter
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "post")
+    private List<Rate> rates;
+}
