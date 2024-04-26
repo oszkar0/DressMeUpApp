5722facef0a8424ad33ef2bcdadcdf75da90c3a4
oszkar0 <oskszysiak@gmail.com>
2024-04-26 17:30:19 +0200

    initial commit

---
+++ DressMeUpAPI/src/main/java/com/dressmeup/DressMeUpAPI/DressMeUpApiApplication.java
@@ -0,0 +1,13 @@
+package com.dressmeup.DressMeUpAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressMeUpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressMeUpApiApplication.class, args);
+	}
+
+}
---
+++ DressMeUpAPI/src/test/java/com/dressmeup/DressMeUpAPI/DressMeUpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dressmeup.DressMeUpAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressMeUpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
