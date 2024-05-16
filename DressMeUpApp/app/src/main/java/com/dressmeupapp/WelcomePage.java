c8caaf6e09c8092290a7d54315c99c2cb7993ef4
bartpob <bartlomiejpoblocki@gmail.com>
2024-05-16 22:07:42 +0200

    login view, navigation to signup

---
+++ DressMeUpApp/app/src/main/java/com/dressmeupapp/WelcomePage.java
@@ -0,0 +1,32 @@
+package com.dressmeupapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void goToSignUp(View view) {
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
+        finish();
+    }
+}
