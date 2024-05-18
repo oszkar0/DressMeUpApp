779517ebcb4f7b51febf5e73f02b7280b3b1e273
oszkar0 <oskszysiak@gmail.com>
2024-05-18 13:34:49 +0200

    make signup page

--- DressMeUpApp/app/src/main/java/com/dressmeupapp/SignUpPage.java
+++ DressMeUpApp/app/src/main/java/com/dressmeupapp/SignUpPage.java
@@ -1,6 +1,8 @@
 package com.dressmeupapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class SignUpPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void goToWelcomePage(View view) {
+        Intent intent = new Intent(this, WelcomePage.class);
+        startActivity(intent);
+        finish();
     }
 }
